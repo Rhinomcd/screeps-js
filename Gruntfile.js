c415193531ad1f3308106725bbe5882e7e0c6e4c
Ryan McDonough <rmcdono@transunion.com>
2020-12-24 20:24:56 +0000

    Add rollup/grunt config we're ready to rock

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,31 @@
 module.exports = function(grunt) {
 
+    grunt.loadNpmTasks('grunt-screeps');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-rollup');
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
         screeps: {
             options: {
                 email: 'rhinomcd@gmail.com',
-                token: process.env.SCREEPS_API_KEY
+                token: process.env.SCREEPS_API_KEY,
+                branch: 'Screeps-js'
             },
-            dist {
+            dist: {
                 src: ['dist/*.js']
             }
         },
         jshint: {
-
+            all: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
+        },
+        rollup: {
+            options: {
+                'sourceMap': 'true'
+            },
+            main: {
+                src: 'src/main.js',
+                dest: 'dist/main.js'
+            }
         }
     });
-    grunt.registerTaks('default', ['jshint']);
+    grunt.registerTask('default', ['jshint', 'rollup', 'screeps']);
 };
